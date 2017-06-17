47891eb97fff375500a032b23fef7a2681b50735
Till Hackler <till.hackler@gmx.com>
2017-06-17 16:40:20 +0100

    Convert time units to seconds, use Alexa slots to set times.

--- hackathon_timer.py
+++ hackathon_timer.py
@@ -12,9 +12,18 @@ ask = Ask(app, '/')
 def hackathon_timer(timeout, timeout_units, notification, notification_units):	
 	text = render_template('hackathon_timer', timeout=timeout, timeout_units=timeout_units,
 		notification=notification, notification_units=notification_units)
-	timekeeper = timer()
-	timekeeper.start()
-	return statement(text).simple_card('Timekeeper', text)
+	# convert times into seconds
+	conversion_factor = { 'hour'   : 3600,
+	                      'hours'  : 3600,
+	                      'minute' : 60,
+	                      'minutes': 60,
+	                      'second' : 1,
+	                      'seconds': 1}
+	timeout = timeout * conversion_factor[timeout_units]
+	notification = notification * conversion_factor[notification_units]
+	presentation_timer = timer(timeout, notification)
+	presentation_timer.start()
+	return statement(text).simple_card('Presentation Timer', text)
 
 @app.route('/start')
 def start():
@@ -29,11 +38,14 @@ def timeout():
 	return render_template('youtube.html')
 
 class timer(Thread):
+	def __init__(self, timeout, notification):
+		self.timeout = timeout
+		self.notification = notification
 	def run(self):
-		webbrowser.open("http://localhost:5000/start", 1)
-		time.sleep(5)
+		webbrowser.open("http://localhost:5000/start")
+		time.sleep(notification)
 		webbrowser.open("http://localhost:5000/notification")
-		time.sleep(5)
+		time.sleep(timeout)
 		webbrowser.open("http://localhost:5000/timeout")
 		
 
